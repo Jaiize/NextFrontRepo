55ab04c4ef87011a507424ec49beb3725286f651
Jaiize <jaiize@outlook.com>
2026-07-24 01:56:18 +0100

    Adds background adjustment with more game details

---
+++ components/spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="w-5 h-5 rounded-full border-2 border-transparent border-t-blue-500 animate-spin"></div>
+    </div>
+  );
+};
+
+export default Spinner;
